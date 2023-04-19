aa39f11a93178c0421dc0f93db08be6c1d81e8fc
seong ho <412jsh@naver.com>
2023-04-19 21:03:00 +0900

    230414

--- src/main/java/idusw/springboot/boradthymleaf/controller/MemberController.java
+++ src/main/java/idusw/springboot/boradthymleaf/controller/MemberController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
 
     MemberService memberService;
-    public MemberController(MemoService memoService) {
+    public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
